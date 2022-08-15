d4a42e3be88e73e214c1441fb20914bf1c2bca65
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-15 16:46:37 -0500

    commit

--- src/main/java/com/nttdata/bootcamp/bank/product/model/document/ProductSubTypeRule.java
+++ src/main/java/com/nttdata/bootcamp/bank/product/model/document/ProductSubTypeRule.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
-@Document(collection="rule")
+@Document(collection="product_subtype_rule")
 public class ProductSubTypeRule
 {
     @Id
